6ab7d926ae2efb3ab842af328f2c97ec900d84e8
bwhite03 <bwhite1195@yahoo.com>
2020-04-09 21:08:21 -0400

    added search bar for side-info

---
+++ client/src/components/util/search-bar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./search-bar.styles.scss";
+
+function SearchBar() {
+  return (
+    <div id="search-bar">
+      <input type="search" placeholder="Search Twitter" />
+    </div>
+  );
+}
+
+export default SearchBar;
--- client/src/pages/homepage/HomePage.js
+++ client/src/pages/homepage/HomePage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Feed from "../../components/feed/Feed";
 import WhoToFollow from "../../components/who-to-follow/WhoToFollow";
+import SearchBar from "../../components/util/search-bar/SearchBar";
 import "./homepage.styles.scss";
 
 function HomePage() {
   return (
     <div id="homepage-container">
       <Feed />
-      <WhoToFollow />
+      <div className="side-info-container">
+        <SearchBar />
+        <WhoToFollow />
+      </div>
     </div>
   );
 }
