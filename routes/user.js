b2b690563fab6e011a5909ebfd65de589b81690c
bwhite03 <bwhite1195@yahoo.com>
2020-05-10 18:37:20 -0400

    added single user route and post user route

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,39 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/users.model");
 
-// Test Routes
+// get all users
 router.get("/users", (req, res) => {
   User.find({}).exec((err, users) => {
     if (err) {
       console.log(err);
     } else {
       res.json(users);
+    }
+  });
+});
+
+// get single user
+router.get("/users/:id", (req, res) => {
+  User.findOne({ _id: req.params.id }).exec((err, user) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(user);
+    }
+  });
+});
+
+router.post("/user", (req, res) => {
+  const newUser = new User();
+  newUser.email = req.body.email;
+  newUser.username = req.body.username;
+  newUser.email = req.body.password;
+
+  newUser.save((err, user) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(user);
     }
   });
 });
