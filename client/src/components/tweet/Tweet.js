328ef035fde39868be865ebcafe990d6bcdffb3a
bwhite03 <bwhite1195@yahoo.com>
2020-06-11 18:16:12 -0400

    adjusted tweet component styles and added icons

--- client/src/components/tweet/Tweet.js
+++ client/src/components/tweet/Tweet.js
@@ -3,26 +3,42 @@ import { Avatar } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import { Link } from "react-router-dom";
 import moment from "moment";
+import ChatBubbleOutlineOutlinedIcon from "@material-ui/icons/ChatBubbleOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@material-ui/icons/FavoriteBorderOutlined";
 import "./tweet.styles.scss";
 
 function Tweet(props) {
   return (
     <Card variant="outlined" id="tweet-container">
-      <div className="info-container">
-        {props.tweet.profileImg ? (
-          <Avatar src={props.tweet.profileImg} alt="userimg" sizes={"lg"} />
-        ) : (
-          <Avatar alt="userimg" sizes={"lg"}>
-            {props.tweet.username.charAt(0).toUpperCase()}
-          </Avatar>
-        )}
-        <Link to={props.tweet.userid}>
-          <p className="username">{props.tweet.username}</p>
-        </Link>
-        <p className="date">{moment(props.tweet.dateCreated).calendar()}</p>
-      </div>
-      <div className="content-container">
-        <p>{props.tweet.content}</p>
+      <div className="containerr">
+        <div className="avatar">
+          {props.tweet.profileImg ? (
+            <Avatar src={props.tweet.profileImg} alt="userimg" sizes={"lg"} />
+          ) : (
+            <Avatar alt="userimg" sizes={"lg"}>
+              {props.tweet.username.charAt(0).toUpperCase()}
+            </Avatar>
+          )}
+        </div>
+        <div>
+          <div className="info-container">
+            <Link to={props.tweet.userid}>
+              <p className="username">{props.tweet.username}</p>
+            </Link>
+            <p className="date">{moment(props.tweet.dateCreated).calendar()}</p>
+          </div>
+          <div className="content-container">
+            <p>{props.tweet.content}</p>
+          </div>
+          <div className="icons">
+            <div className="comment-icon">
+              <ChatBubbleOutlineOutlinedIcon /> <p>3</p>
+            </div>
+            <div className="heart-icon">
+              <FavoriteBorderOutlinedIcon /> <p>3</p>
+            </div>
+          </div>
+        </div>
       </div>
     </Card>
   );
